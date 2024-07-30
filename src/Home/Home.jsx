e0d3d83222456506ad2fb4e72f50889f03af97a3
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-07-30 13:03:24 +0600

    added to about section

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,6 +1,7 @@
 import About from "../About/About";
 import Banner from "../Banner/Banner";
 import Navbar from "../Navbar/Navbar";
+import Technologies from "../Technologies/Technologies";
 
 const Home = () => {
     return (
@@ -10,10 +11,11 @@ const Home = () => {
             <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
             </div>
             
-                    <div className="max-w-screen-xl mx-auto">
+                    <div className="max-w-screen-xl mx-auto px-8">
                     <Navbar></Navbar>
                     <Banner></Banner>
                     <About></About>
+                    <Technologies></Technologies>
                     </div>
                 
             </div>
--- src/Technologies/Technologies.jsx
+++ src/Technologies/Technologies.jsx
@@ -1,11 +1,37 @@
+import { DiRedis } from "react-icons/di";
+import { FaNode, FaPython, FaReact } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
+import { SiNextdotjs } from "react-icons/si";
+
 
 
 const Technologies = () => {
     return (
         <>
-        <div>
-            
-        </div>
+            <div className="border-b border-neutral-800 pb-24">
+                <h1 className="my-20 text-center text-4xl">Technologies</h1>
+                <div className="flex flex-wrap items-center justify-center gap-4">
+                    <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                        <FaReact className="text-7xl text-cyan-400" />
+                    </div>
+                    <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                        <SiNextdotjs className="text-7xl text-white" />
+                    </div>
+                    <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                        <FaNode className="text-7xl text-green-400" />
+                    </div>
+                    <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                        <IoLogoJavascript className="text-7xl  bg-yellow-400  text-gray-800" />
+                    </div>
+                    <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                        <FaPython className="text-7xl text-yellow-400" />
+                    </div>
+                    <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                        <DiRedis className="text-7xl text-red-600" />
+                    </div>
+                    
+                </div>
+            </div>
         </>
     );
 };
