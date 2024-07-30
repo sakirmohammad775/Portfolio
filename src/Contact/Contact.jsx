7243771fff9d99c734b7b7c5bc4462864aab2775
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-07-31 00:35:22 +0600

    addcontact file

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -6,6 +6,7 @@ const Contact = () => {
             <h1 className="my-10 text-center text-4xl">Get in Touch</h1>
             <div className="text-center tracking-tighter">
                 <p className="my-4">Chattogram Bangladesh</p>
+                <p className="my-2">089473879437</p>
             </div>
         </div>
     );
