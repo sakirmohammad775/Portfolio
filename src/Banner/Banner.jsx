d4490aa7a16d007fc488c933905ceaa85e8c0c74
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-07-31 10:07:04 +0600

    add the framer motion

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -1,5 +1,5 @@
 import bannerPicture from '../assets/kevinRushProfile.png'
-
+import { motion } from "framer-motion"
 const Banner = () => {
     return (
         <>
@@ -7,7 +7,7 @@ const Banner = () => {
                 <div className="flex flex-wrap">
                     <div className="w-full lg:flex ">
                         <div className="flex flex-col lg:w-1/2 items-center lg:items-start">
-                            <h1 className="text-4xl pb-16 font-thin tracking-tight lg:mt-16 lg:text-6xl">Sakir Mohammad</h1>
+                            <motion.h1 animate={{ x: 100 }} className="text-4xl pb-16 font-thin tracking-tight lg:mt-16 lg:text-6xl">Sakir Mohammad</motion.h1>
                             <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Full stack Developer</span>
                             <p className="my-2 mx-w-xl py-6 font-light tracking-tight ">I am a JavaScript full-stack developer with expertise in both front-end and back-end development. I work with technologies like React, Angular, or Vue.js on the front end and Node.js, Express, or similar on the back end. My skills include creating responsive user interfaces, building APIs, managing databases, and ensuring application security and performance. I thrive in collaborative settings, working closely with teams to deliver high-quality, scalable web applications.</p>
                         </div>
