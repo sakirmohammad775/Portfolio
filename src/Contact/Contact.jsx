c30f749fdf02880cec341a066daa54c476a850c2
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-07-31 12:17:05 +0600

    addframer in project,banner etc

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,12 +1,20 @@
-
+import {  motion } from "framer-motion"
 
 const Contact = () => {
     return (
         <div className="border-2 border-neutral-900 pb-20">
             <h1 className="my-10 text-center text-4xl">Get in Touch</h1>
             <div className="text-center tracking-tighter">
-                <p className="my-4">Chattogram Bangladesh</p>
-                <p className="my-2">089473879437</p>
+                <motion.p 
+                whileInView={{opacity:1,x:0}}
+                initial={{opacity:0,x:100}}
+                transition={{duration:1}}
+                className="my-4">Chattogram Bangladesh</motion.p>
+                <motion.p  
+                whileInView={{opacity:1,x:0}}
+                initial={{opacity:0,x:-100}}
+                transition={{duration:1}}
+                className="my-2">089473879437</motion.p>
             </div>
         </div>
     );
