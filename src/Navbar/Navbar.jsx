059c43ef643515d7a368d95d5ba56adccfa404eb
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-03 13:19:29 +0600

    rename project

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { FaFacebook, FaGithub, FaInstagram } from "react-icons/fa";
 const Navbar = () => {
     return (
         <div className="flex justify-between  py-8">
-           <div className="text-2xl">Levis</div>
+           <div className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">SM</div>
            <div className="flex gap-2 text-2xl">
            <FaInstagram />
            <FaFacebook/>
